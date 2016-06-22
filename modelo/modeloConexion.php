20992ffa0985d9d5820b119842b3cb5d49496a46
diego890 <frankdiego1993@gmail.com>
2016-06-22 16:56:03 -0500

    Update modeloConexion.php

--- modelo/modeloConexion.php
+++ modelo/modeloConexion.php
@@ -5,7 +5,7 @@ class Conexion_Model {
 
     public static function getConexion() {
     
-        $conexion = @mysql_connect("tcp:diego9309.database.windows.net,1433","diego@diego9309.database.windows.net","17907660*+24D") or die("Conexion Fallida");
+        $conexion = @mysql_connect("eu-cdbr-azure-north-e.cloudapp.net","bb3f6484d31761","63fc0c61") or die("Conexion Fallida");
 	@mysql_select_db("proceso",$conexion)or die("Error cargando la base de datos");    
         return $conexion;
     }
