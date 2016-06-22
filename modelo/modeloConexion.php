402185b81f419e3121394eba0a03ca0e822ab98c
diego890 <frankdiego1993@gmail.com>
2016-06-22 16:43:11 -0500

    Update modeloConexion.php

--- modelo/modeloConexion.php
+++ modelo/modeloConexion.php
@@ -6,7 +6,7 @@ class Conexion_Model {
     public static function getConexion() {
     
         $conexion = @mysql_connect("diego9309.database.windows.net","diego","17907660*+24D") or die("Conexion Fallida");
-	@mysql_select_db("procesos",$conexion)or die("Error cargando la base de datos");    
+	@mysql_select_db("proceso",$conexion)or die("Error cargando la base de datos");    
         return $conexion;
     }
 
